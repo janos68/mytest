8e31ffa494c7bb7d5264dab98f55846de66c3678
Janos <kocsisjanos68@gmail.com>
2021-02-23 22:47:49 +0100

    version 1.1

---
+++ mavenproject2/src/main/java/com/infoking/mavenproject2/testjava11.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infoking.mavenproject2;
+
+/**
+ *
+ * @author KJanos
+ */
+public class testjava11 {
+    private int i;
+    
+}
